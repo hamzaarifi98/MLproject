b1ce75d3706380868a7d319a9f44b384dc7a460f
Hamza <hamzaa.arifii@gmail.com>
2025-05-24 02:55:35 +0200

    Data transformation model

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,11 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+
+
 
 @dataclass
 
@@ -23,7 +28,7 @@ class DataIngestion:
         logging.info("Enter the data ingestion method or component")
         try:
             #Read data set 
-            df=pd.read_csv("notebook\data\stud.csv")
+            df=pd.read_csv("notebook/data/stud.csv")
             logging.info("Read the dataset as data frame")
 
             #Create folders or path where it is going to be saved
@@ -50,4 +55,8 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()    
+    train_data, test_data = obj.initiate_data_ingestion()   
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
+     
